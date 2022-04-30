17c1ca8ca33db89bd3b83b9bfc687c692618b035
GustavoCarv <gustavo.carvalho@econverse.com.br>
2022-04-30 20:14:46 -0300

    add: estrutura e estilização da página de login-desk/mobile

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,23 +4,23 @@ import {goToLogin} from "../../routes/coordinator";
 
 //STYLES
 import { HomeContainer } from "./styled";
-import reciclyingSVG from "../../assets/icons/recycling.svg";
+import logo from "../../assets/icons/logo.svg";
 import {Zoom} from "@material-ui/core";
 
 export const HomePage = () => {
   const navigate = useNavigate()
 
   useEffect(() => {
-    //setTimeout(()=> goToLogin(navigate), 3000)
+    setTimeout(()=> goToLogin(navigate), 3000)
   }, [])
   
   
 
   return (
     <HomeContainer>
-      <Zoom in style={{transitionDelay: reciclyingSVG ? '1000ms' : '0ms'}}>
+      <Zoom in style={{transitionDelay: logo ? '1000ms' : '0ms'}}>
       <div>
-        <img width={120} height={120} src={reciclyingSVG} alt="Reciclável" />
+        <img width={120} height={120} src={logo} alt="Reciclável" />
         <h1> Recoleta </h1>
       </div>
       </Zoom>
