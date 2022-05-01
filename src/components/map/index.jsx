6d2eea06f0a2afa691b2c664c1f0332f3400a464
Mônica S <monica.s.bergues@gmail.com>
2022-04-30 22:06:24 -0300

    feat: geolocation on map

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -11,6 +11,28 @@ export const Maps = ({ MaterialType }) => {
     const data = Data.users.filter((elem) => elem.properties.TYPE === material);
     SetFilterInfo(data);
   };
+
+  const [location, setLocation] = React.useState({
+    loaded: false,
+    coordinates: { lat: "", lng: "" },
+  });
+  console.log(location.coordinates.lat);
+
+  const onSuccess = (location) => {
+    setLocation({
+      loaded: true,
+      coordinates: {
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      },
+    });
+  };
+  const onError = (error) => {
+    setLocation({
+      loaded: true,
+      error,
+    });
+  };
   React.useEffect(() => {
     if (MaterialType === "paper") {
       setType("paper");
@@ -18,12 +40,26 @@ export const Maps = ({ MaterialType }) => {
     if (MaterialType === "glass") {
       setType("glass");
     }
+    if (!("geolocation" in navigator)) {
+      onError({
+        code: 0,
+        message: "Geolocation not supported",
+      });
+    }
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
   }, []);
 
   console.log(filterInfo);
   return (
     <Container>
-      <MapContainer center={[-23.99482, -46.256901]} zoom={12}>
+      <MapContainer
+        center={
+          location
+            ? [location.coordinates.lat, location.coordinates.lng]
+            : [-23.99482, -46.256901]
+        }
+        zoom={12}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
