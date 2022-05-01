60ccbb16e3bb1470a88738fd6dd642eef0ea9e24
GustavoCarv <gustavo.carvalho@econverse.com.br>
2022-04-30 21:40:21 -0300

    add: barra do final adicionada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,24 @@
-export const HomePage = () => {
-    return (
-        <div>
-        <h1>Home</h1>
-        </div>
-    );
-} 
+import { MapPage } from "../../components/map/index";
 
+// STYLES
+import { Container, BottomBar, BottomBarItem } from "./style";
+import recycling from "../../assets/icons/recycling.svg";
+import handshake from "../../assets/icons/handshake.svg";
+
+export const HomePage = () => {
+  return (
+    <Container>
+      <MapPage MatiralType="glass" />
+      <BottomBar> 
+        <BottomBarItem color>
+            <img width={24} height={24} src={recycling} alt="ícone da coleta"/> 
+            <p> Coletar </p>
+        </BottomBarItem>
+        <BottomBarItem>
+            <img width={24} height={24} src={handshake} alt="ícone de doação"/>
+            <p> Doar </p>
+        </BottomBarItem>
+      </BottomBar>
+    </Container>
+  );
+};
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -2,5 +2,31 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   height: 100vh;
+  position: relative;
+`;
 
+export const BottomBar = styled.div`
+  position: fixed;
+  width: 100%;
+  z-index: 999;
+  bottom: 0;
+  padding-bottom: 2rem;
+  padding-top: 1rem;
+  background-color: white;
+
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const BottomBarItem = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+
+  p {
+    font-size: 1rem;
+    font-family: "Poppins", sans-serif;
+    color: ${props => props.color ? "var(--success)" : "black"};
+  }
 `;
