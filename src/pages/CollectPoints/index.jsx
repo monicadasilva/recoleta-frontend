b7501ef1e4fa7986d99cd53a16d0479d98a776d1
Douglas Filipe <douglasfelipe.net77@gmail.com>
2022-05-01 14:41:22 -0400

    feat: add donorDetails

--- src/pages/CollectPoints/index.jsx
+++ src/pages/CollectPoints/index.jsx
@@ -1,5 +1,5 @@
 import { Maps } from "../../components/map";
-import { FootBar, Content, Headers } from "./styled";
+import { Headers } from "./styled";
 import { Menu } from "../../components/menu";
 import Person from "../../assets/icons/person.svg";
 import { ReactComponent as MenuIcon } from "../../assets/icons/menu.svg";
@@ -7,12 +7,14 @@ import { ReactComponent as Notf } from "../../assets/icons/notification.svg";
 
 import React from "react";
 import { MenuDown } from "../../components/MenuDown";
+import { DonorDetails } from "../../components/DonorDetails";
 
 export const Collect = () => {
   const [opens, setOpen] = React.useState(false);
   console.log(opens);
   return (
     <>
+      <DonorDetails />
       <MenuDown />
       <span onClick={() => setOpen(false)}>
         <Maps MaterialType={"paper"} />
