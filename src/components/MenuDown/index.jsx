24541e0c2a757a6861248c1a879020bec02d9d9e
Douglas Filipe <douglasfelipe.net77@gmail.com>
2022-05-01 20:32:36 -0400

    fix: create donation

--- src/components/MenuDown/index.jsx
+++ src/components/MenuDown/index.jsx
@@ -1,28 +1,36 @@
 import { Container } from "./styles";
 import { MdRecycling } from "react-icons/md";
 import { FaHandsHelping } from "react-icons/fa";
-import { useMenu } from '../../Contexts/menu'
+import { useMenu } from "../../Contexts/menu";
+import { useNavigate } from "react-router-dom";
+export const MenuDown = ({ open }) => {
+  const { SetMenuDown, SetMenuColletc } = useMenu();
+  const navigate = useNavigate();
 
-export const MenuDown = ( { open } ) => {
-
-  const { SetMenuDown, SetMenuColletc } = useMenu()
+  const donationClick = () => {
+    navigate("/create-donation");
+    SetMenuDown(false);
+  };
 
   return (
     <>
-    { open && 
-      <Container>
-        <div className="menusAction">
-          <div className="collect" onClick={() => (SetMenuDown(false), SetMenuColletc(true))} >
-            <MdRecycling className="svg rec" />
-            <p>Coletar</p>
+      {open && (
+        <Container>
+          <div className="menusAction">
+            <div
+              className="collect"
+              onClick={() => (SetMenuDown(false), SetMenuColletc(true))}
+            >
+              <MdRecycling className="svg rec" />
+              <p>Coletar</p>
+            </div>
+            <div className="donation" onClick={donationClick}>
+              <FaHandsHelping className="svg hands" />
+              <p>Doar</p>
+            </div>
           </div>
-          <div className="donation" onClick={() => SetMenuDown(false)}>
-            <FaHandsHelping className="svg hands" />
-            <p>Doar</p>
-          </div>
-        </div>
-      </Container>
-      }
+        </Container>
+      )}
     </>
   );
 };
